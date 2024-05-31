422d3240b659f42e36aae435b16a7136409ec161
febry40 <febryevayonisa@gmail.com>
2024-05-31 14:49:31 +0700

    jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -45,4 +45,10 @@ void remove() {
         FRONT = -1;
         REAR = -1;
     }
+    else {
+        if (FRONT == max - 1)
+        FRONT = 0;
+        else 
+            FRONT = FRONT + 1;
+    }
 }
